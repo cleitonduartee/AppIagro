f96c029edfd9ec47b00fdf9cabbf2a45b7e537e4
cleitonduartee <djcleitonduarte@hotmail.com>
2022-02-14 08:06:26 -0400

    Ajustado funcao de cancelar registro

---
+++ AppIAGRO/Controllers/VendasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppIAGRO.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
