ad8445648cc36232cb7105b972788d1868233631
cleitonduartee <djcleitonduarte@hotmail.com>
2022-02-17 14:02:02 -0400

    Criado camada Services para Animais

---
+++ AppIAGRO/wwwroot/app/animais/app.animais.services.js
@@ -0,0 +1,128 @@
+﻿var app = app || {};
+app.animais = app.animais || {};
+
+app.animais.services = function () {
+
+    const header = new Headers({
+        "Content-Type": "application/json",
+        "X-Custom-Header": "ProcessThisImmediately",
+    });
+
+    var BuscarPropriedadesNaApi = function () {
+        return fetch('http://localhost:62978/api/v1/Propriedade/BuscarTodos')
+            .then(async resp => {
+                if (resp.ok)
+                    return resp.json();
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    var BuscarProdutoresNaApi = function () {
+        return fetch('http://localhost:62978/api/v1/Produtor/BuscarTodos')
+            .then(async resp => {
+                if (resp.ok)
+                    return resp.json();
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+    var BuscarAnimaisPorNomePropriedadeNaApi = function (nomePropriedade) {
+        const url = `http://localhost:62978/api/v1/Animais/BuscarPorPropriedade/${nomePropriedade}`;
+        return fetch(url)
+            .then(async resp => {
+                if (resp.ok)
+                    return resp.json();
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    var BuscarAnimaisPorNomeProdutorNaApi = function (nomeProdutor) {
+        const url = `http://localhost:62978/api/v1/Animais/BuscarPorProdutor/${nomeProdutor}`;
+        return fetch(url)
+            .then(async resp => {
+                if (resp.ok)
+                    return resp.json();
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    var BuscarEntradaDeAnimaisNaApi = function (idPropriedade) {
+        const url = ` http://localhost:62978/api/v1/Animais/BuscarEntradasPorPropriedadeId/${idPropriedade} `;
+        return fetch(url)
+            .then(async resp => {
+                if (resp.ok)
+                    return resp.json();
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    var RealizaEntradaDeAnimaisNaApi = function (body) {
+        const url = ` http://localhost:62978/api/v1/Animais/EntradaAnimais `;
+        const options = {
+            method: 'POST',
+            mode: 'cors',
+            headers: header,
+            body: JSON.stringify(body)
+        }
+        return fetch(url, options)
+            .then(async resp => {
+                if (resp.ok)
+                    return true;
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    var CancelarEntradaDeAnimaisNaApi = function (codigoEntrada) {
+        const url = ` http://localhost:62978/api/v1/Animais/CancelamentoEntradaAnimais/${codigoEntrada} `;
+        const options = {
+            method: 'POST',
+            mode: 'cors',
+            headers: header,
+            body: {}
+        }
+        return fetch(url, options)
+            .then(async resp => {
+                if (resp.ok)
+                    return true;
+                else
+                    throw new Error(await resp.json());
+            })
+            .catch(error => {
+                alert(error.message)
+            });
+    };
+
+    return {
+        BuscarPropriedadesNaApi: BuscarPropriedadesNaApi,
+        BuscarProdutoresNaApi: BuscarProdutoresNaApi,
+        BuscarAnimaisPorNomePropriedadeNaApi: BuscarAnimaisPorNomePropriedadeNaApi,
+        BuscarAnimaisPorNomeProdutorNaApi: BuscarAnimaisPorNomeProdutorNaApi,
+        RealizaEntradaDeAnimaisNaApi: RealizaEntradaDeAnimaisNaApi,
+        BuscarEntradaDeAnimaisNaApi: BuscarEntradaDeAnimaisNaApi,
+        CancelarEntradaDeAnimaisNaApi: CancelarEntradaDeAnimaisNaApi
+    }
+
+}();
--- AppIAGRO/wwwroot/app/animais/app.animais.viewModel.js
+++ AppIAGRO/wwwroot/app/animais/app.animais.viewModel.js
@@ -1,6 +1,7 @@
 ﻿var app = app || {};
 app.animais = app.animais || {};
 app.animais.model = app.animais.model || {};
+app.animais.services = app.animais.services || {};
 
 app.produtor = app.produtor || {};
 app.produtor.model = app.produtor.model || {};
@@ -12,11 +13,9 @@ app.historico.model = app.historico.model || {};
 
 app.propriedade.viewModel = function () {
 
-    const header = new Headers({
-        "Content-Type": "application/json",
-        "X-Custom-Header": "ProcessThisImmediately",
-    });
+    
     var model = app.animais.model;
+    var services = app.animais.services;
     var modelProdutor = app.produtor.model;
     var modelPropriedade = app.propriedade.model;
     var modelHistorico = app.historico.model;
@@ -24,161 +23,54 @@ app.propriedade.viewModel = function () {
     ko.applyBindings(new function () {
         var self = this;
 
-        self.BuscarPropriedadesNaApi = function () {
-            fetch('http://localhost:62978/api/v1/Propriedade/BuscarTodos')
-                .then(p => p.json())
-                .then(listPropriedade => {
-                    listPropriedade.forEach(function (item) {
-                        self.propriedades.push(new modelPropriedade.Propriedade(item));
-                    });
-                })
-        };
-        self.BuscarProdutoresNaApi = function () {            
-            fetch('http://localhost:62978/api/v1/Produtor/BuscarTodos')
-                .then(p => p.json())
-                .then(listProdutores => {
-                    listProdutores.forEach(function (item) {
-                        self.produtores.push(new modelProdutor.Produtor(item));
-                    });
-                })
-        };
-        
-        self.BuscarPropriedadesNaApi();
-        self.BuscarProdutoresNaApi();
-
-        self.BuscarAnimaisPorNomePropriedadeNaApi = function (nomePropriedade) {           
-            const url = `http://localhost:62978/api/v1/Animais/BuscarPorPropriedade/${nomePropriedade}`;
-            fetch(url)
-                .then(p => p.json())
-                .then(animais => {                    
-                    self.animaisPorPropriedade.removeAll();
-                    self.animaisPorPropriedade.push(new model.Animal(animais));
-                })
-        };
-
-        self.BuscarAnimaisPorNomeProdutorNaApi = function (nomeProdutor) {
-            const url = `http://localhost:62978/api/v1/Animais/BuscarPorProdutor/${nomeProdutor}`;
-            fetch(url)
-                .then(p => p.json())
-                .then(listAnimais => {                    
-                    self.animaisPorProdutor.removeAll();
-                    listAnimais.forEach(item => {
-                        self.animaisPorProdutor.push(new model.Animal(item));
-                    });
-                })
-        };
-
-        self.RealizaEntradaDeAnimaisNaApi = function (body) {
-            const url = ` http://localhost:62978/api/v1/Animais/EntradaAnimais `;
-            const options = {
-                method: 'POST',
-                mode: 'cors',
-                headers: header,
-                body: JSON.stringify(body)
-            }
-            return fetch(url, options)
-                .then(resp => {
-                    if (resp.ok) {                       
-                        alert("Entrada de animais realizada com sucesso.");
-                        $('#dialogEntraAnimais').modal('hide');
-                        self.ResetVariaveis();
-                    } else {
-                        alert('Erro ao realizar entrada de animais. Tente novamente.');
-                    }
-                })
-                .catch(e => {
-                    alert(e);
-                })
-        };
-
-        self.BuscarEntradaDeAnimaisNaApi = function (idPropriedade) {
-            const url = ` http://localhost:62978/api/v1/Animais/BuscarEntradasPorPropriedadeId/${idPropriedade} `;
-            fetch(url)
-                .then(p => p.json())
-                .then(listEntradas => {
-                    self.HistoricoEntradas.removeAll();
-                    listEntradas.forEach(item => {
-                        self.HistoricoEntradas.push(new modelHistorico.HistoricoEntrada(item));
-                    });
-                })
-        };
-        self.CancelarEntradaDeAnimaisNaApi = function (codigoEntrada) {
-            const url = ` http://localhost:62978/api/v1/Animais/CancelamentoEntradaAnimais/${codigoEntrada} `;
-            const options = {
-                method: 'POST',
-                mode: 'cors',
-                headers: header,
-                body: {}
-            }
-            return fetch(url, options)
-                .then(async resp => {
-                    if (resp.ok) {
-                        alert("Entrada cancelada com sucesso.");
-                        $('#dialogCancelarEntraAnimais').modal('hide');
-                        self.ResetVariaveis();
-                    } else {
-                        throw new Error(await resp.json());
-                    }
-                })
-                .catch(error => {
-                    alert(error.message);
-                })
-        };
-       
+        self.QtdBovino = ko.observable();
+        self.QtdBubalino = ko.observable();
         self.NomePropriedade = ko.observable();
-        self.NomePropriedade.subscribe(function (nomePropriedade) {
-            if (nomePropriedade != undefined)
-                self.BuscarAnimaisPorNomePropriedadeNaApi(nomePropriedade);
-
-            self.animaisPorPropriedade.removeAll();
-            
-        });
-
         self.NomeProdutor = ko.observable();
-        self.NomeProdutor.subscribe(function (nomeProdutor) {
-            if (nomeProdutor != undefined)
-                self.BuscarAnimaisPorNomeProdutorNaApi(nomeProdutor);
-
-            self.animaisPorProdutor.removeAll();
-
-        });
         self.PropriedadeId = ko.observable();
-        self.PropriedadeId.subscribe(function (idProdutor) {
-            if (idProdutor != undefined)
-                self.BuscarEntradaDeAnimaisNaApi(idProdutor);
+       
 
-            self.HistoricoEntradas.removeAll();
-
-        });
-
-        
         self.isEditing = ko.observable(false);
 
+        self.HistoricoEntradas = ko.observableArray();
         self.animaisPorPropriedade = ko.observableArray();
         self.animaisPorProdutor = ko.observableArray();
         self.produtores = ko.observableArray();
         self.propriedades = ko.observableArray();
+        self.propriedadesCopyParaEntradaAnimais = ko.observableArray();
 
-
-        self.propriedadesCopyParaEntradaAnimais = ko.observableArray();        
-        self.QtdBovino = ko.observable();
-        self.QtdBubalino = ko.observable();
-        self.HistoricoEntradas = ko.observableArray();
+        /**
+        *  FUNÇÕES AUXILIAR VM
+        */
 
         self.preparaEntradaAnimais = function () {
             self.propriedadesCopyParaEntradaAnimais(self.propriedades());
         };
-        self.RealizaEntradaAnimais = function () {
-            if (!self.EntradaEValida())
-                return;
 
-            self.RealizaEntradaDeAnimaisNaApi({
-                propriedadeId: self.PropriedadeId(),
-                saldoSemVacinaBovino: self.QtdBovino(),
-                saldoSemVacinaBubalino: self.QtdBubalino()
-            })
+        self.EntradaEValida = function () {
+            var validacao = "";
 
-        };
+            if (self.PropriedadeId() == undefined)
+                validacao += "* Propriedade é obrigatória. \n"
+            if (self.QtdBovino() == undefined && self.QtdBubalino() == undefined)
+                validacao += "* Informe a quantidade de animais Bovino ou/e Bubalino \n"
+            if (self.QtdBovino() <= 0 && self.QtdBubalino() <= 0)
+                validacao += "* Quantidade de animais deve ser maior que zero."
+
+            if (validacao) {
+                alert(validacao);
+                return false;
+            }
+            return true;
+        }
+        self.ResetVariaveis = function () {
+            self.NomeProdutor(undefined);
+            self.NomePropriedade(undefined);
+            self.QtdBovino(undefined);
+            self.QtdBubalino(undefined);
+            self.PropriedadeId(undefined);
+
+        }
         self.produtorTemRebanho = ko.computed(function () {
             if (self.NomeProdutor() != undefined) {
                 if (self.animaisPorProdutor().length > 0)
@@ -199,33 +91,141 @@ app.propriedade.viewModel = function () {
             else
                 return true;
         })
+
+        /**
+        *  FUNÇÕES QUE CHAMAM AS FUNCOES QUE UTILIZAM A SERVICE
+        *  -----------   subscribe ----------------------------
+        */
+       
+       
+        self.NomePropriedade.subscribe(function (nomePropriedade) {
+            if (nomePropriedade != undefined)
+                self.BuscarAnimaisPorNomePropriedadeNaApi(nomePropriedade);
+
+            self.animaisPorPropriedade.removeAll();
+            
+        });
+
+       
+        self.NomeProdutor.subscribe(function (nomeProdutor) {
+            if (nomeProdutor != undefined)
+                self.BuscarAnimaisPorNomeProdutorNaApi(nomeProdutor);
+
+            self.animaisPorProdutor.removeAll();
+
+        });
+        
+        self.PropriedadeId.subscribe(function (idProdutor) {
+            if (idProdutor != undefined)
+                self.BuscarEntradaDeAnimaisNaApi(idProdutor);
+
+            self.HistoricoEntradas.removeAll();
+
+        });
+
+        self.RealizaEntradaAnimais = function () {
+            if (!self.EntradaEValida())
+                return;
+
+            var body = {
+                propriedadeId: self.PropriedadeId(),
+                saldoSemVacinaBovino: self.QtdBovino(),
+                saldoSemVacinaBubalino: self.QtdBubalino()
+            }
+
+            self.RealizaEntradaDeAnimaisNaApi(body);
+
+        };
+        
         self.RealizaCancelamento = function (historico) {
             self.CancelarEntradaDeAnimaisNaApi(historico.CodigoHistorico());
         }
-        self.EntradaEValida = function () {
-            var validacao = "";
 
-            if (self.PropriedadeId() == undefined)
-                validacao += "* Propriedade é obrigatória. \n"
-            if (self.QtdBovino() == undefined && self.QtdBubalino() == undefined)
-                validacao += "* Informe a quantidade de animais Bovino ou/e Bubalino \n"
-            if (self.QtdBovino() <= 0 && self.QtdBubalino() <= 0)
-                validacao += "* Quantidade de animais deve ser maior que zero."
+        /**
+         *  FUNÇÕES QUE UTILIZA SERVICES
+         */
 
-            if (validacao) {
-                alert(validacao);
-                return false;
-            }
-            return true;            
-        }
-        self.ResetVariaveis = function () {
-            self.NomeProdutor('');
-            self.NomePropriedade('');
-            self.QtdBovino('');
-            self.QtdBubalino('');
-            self.PropriedadeId('');
+        self.RealizaEntradaDeAnimaisNaApi = function (body) {
+            services.RealizaEntradaDeAnimaisNaApi(body)
+                .then(resp => {
+                    if (resp) {
+                        alert("Entrada de animais realizada com sucesso.");
+                        $('#dialogEntraAnimais').modal('hide');
+                        self.ResetVariaveis();
+                    }
+                })
+        };
 
-        }
+
+        self.CancelarEntradaDeAnimaisNaApi = function (codigoEntrada) {
+            services.CancelarEntradaDeAnimaisNaApi(codigoEntrada)
+                .then(async resp => {
+                    if (resp) {
+                        alert("Entrada cancelada com sucesso.");
+                        $('#dialogCancelarEntraAnimais').modal('hide');
+                        self.ResetVariaveis();
+                    }
+                })
+        };
+     
+        self.BuscarAnimaisPorNomePropriedadeNaApi = function (nomePropriedade) {
+            services.BuscarAnimaisPorNomePropriedadeNaApi(nomePropriedade)
+                .then(animais => {
+                    if (animais) {
+                        self.animaisPorPropriedade.removeAll();
+                        self.animaisPorPropriedade.push(new model.Animal(animais));
+                    }
+                })
+        };
+
+        self.BuscarAnimaisPorNomeProdutorNaApi = function (nomeProdutor) {
+            services.BuscarAnimaisPorNomeProdutorNaApi(nomeProdutor)
+                .then(listAnimais => {
+                    if (listAnimais) {
+                        self.animaisPorProdutor.removeAll();
+                        listAnimais.forEach(item => {
+                            self.animaisPorProdutor.push(new model.Animal(item));
+                        });
+                    }
+                })
+        };
+        self.BuscarEntradaDeAnimaisNaApi = function (idPropriedade) {
+            services.BuscarEntradaDeAnimaisNaApi(idPropriedade)
+                .then(listEntradas => {
+                    if (listEntradas) {
+                        self.HistoricoEntradas.removeAll();
+                        listEntradas.forEach(item => {
+                            self.HistoricoEntradas.push(new modelHistorico.HistoricoEntrada(item));
+                        });
+                    }
+                })
+
+        };
+
+        self.BuscarPropriedadesNaApi = function () {
+            services.BuscarPropriedadesNaApi()
+                .then(listPropriedade => {
+                    if (listPropriedade) {
+                        listPropriedade.forEach(function (item) {
+                            self.propriedades.push(new modelPropriedade.Propriedade(item));
+                        });
+                    }
+                })
+        };
+        self.BuscarProdutoresNaApi = function () {
+            services.BuscarProdutoresNaApi()
+                .then(listProdutores => {
+                    if (listProdutores) {
+                        listProdutores.forEach(function (item) {
+                            self.produtores.push(new modelProdutor.Produtor(item));
+                        });
+                    }
+                })
+
+        };
+
+        self.BuscarPropriedadesNaApi();
+        self.BuscarProdutoresNaApi();
 
     });
 
